51ed462293425b45a0acae408c975ce2c267054e
edisantos <edinaldolemos@yahoo.com.br>
2022-01-09 09:49:25 -0300

    Criação da camada de UI

---
+++ lemossolucoestecnologia.ecommerce.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lemossolucoestecnologia.ecommerce.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
